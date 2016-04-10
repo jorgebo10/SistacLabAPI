d248d5d60c3c010e5dc1ff661143785a58e3cf67
Jorge Bo <jorgebo10@gmail.com>
2016-04-09 21:14:11 -0300

    Add underscore and chai assertions to use BDD and should sintax

--- app/api/specs/a1/a1.controller.spec.js
+++ app/api/specs/a1/a1.controller.spec.js
@@ -2,29 +2,28 @@
 
 'use strict';
 
-var assert = require('assert');
 var sinon = require('sinon');
 require('sinon-as-promised');
 require('sinon-mongoose');
+var should = require('chai').should();
 
 describe('A1ModelController', function() {
 	var A1Controller = require('../../src/a1/a1.controller.js');
 	var A1Model = require('../../src/a1/a1.model.js');
 	
-	it('#controller.findByCit', function(done) {
+	it('should return A1 by CIT', function(done) {
 	
-		var mock = sinon.mock(A1Model);
+	var mock = sinon.mock(A1Model);
 
-    var statusCallback = function(data) {
-			console.log(data);
+    var statusCallback = function(status) {
+			status.should.equal(200);
 		};
 
-    var jsonCallback = function(data) {
-			console.log(data);
+    var jsonCallback = function(json) {
+			json.should.equal('success');
 			mock.restore();
 			done();		
 		};
-
 	
 		var req = { query: {cit: '1'}};
 		var res = { 

7220776bb4940e29d50ac51b3133432142b5c1fa
Jorge <jorgebo10@gmail.com>
2016-06-30 22:20:43 -0300

    Remove warning function about env not set which is not being used

--- config/environment/index.js
+++ config/environment/index.js
@@ -5,13 +5,6 @@
 var path = require('path');
 var _ = require('underscore');
 
-function requiredProcessEnv(name) {
-  if(!process.env[name]) {
-    throw new Error('You must set the ' + name + ' environment variable');
-  }
-  return process.env[name];
-}
-
 // All configurations will extend these options
 // ============================================
 var all = {
